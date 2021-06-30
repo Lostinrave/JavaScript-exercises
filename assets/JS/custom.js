baee3e8b459c65213a316d5cfa09421ea647ddff
Lostinrave <benaskalvinas@gmail.com>
2021-06-30 19:35:07 +0300

    class work exercises

---
+++ assets/JS/custom.js
@@ -0,0 +1,32 @@
+// document.getElementById("enterValue").addEventListener("click", toCart);
+// function toCart() {
+//   var cartValue = document.getElementById("cartValue").value;
+//   var word = "items";
+
+//   getElementById("valueReturn").innerHTML =
+//     "You succesfuly added " + cartValue + " " + word + "to your shoping cart";
+// }
+
+document.getElementById('kvietejas').addEventListener("click", ikrepseli);
+
+function ikrepseli() {
+
+    var kiekis = document.getElementById('kiekis').value;
+    var zodis  = 'prekes';
+
+    if(kiekis%10 == 1) {
+        zodis  = 'prekę';
+    }
+
+    if( (kiekis > 10 && kiekis < 20) || kiekis%10 == 0) {
+        zodis = 'prekių';
+    } 
+
+    if ((kiekis%100 == 11) || (kiekis%100 < 21 )){
+        zodis = 'prekių';
+    }
+
+    document.getElementById('messages').innerHTML = 'Jūs sėkmingai pridėjote ' + kiekis + ' ' + zodis + '  į savo krepšelį!';
+
+    //alert('Jūs sėkmingai pridėjote ' + kiekis + ' prekę į savo krepšelį!');
+}
