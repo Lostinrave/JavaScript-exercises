5112bd2652a8b3cf78712915bc388b637dd370ec
Lostinrave <83598946+Lostinrave@users.noreply.github.com>
2021-07-01 22:03:19 +0300

    added jquery and datatables table

--- assets/JS/custom.js
+++ assets/JS/custom.js
@@ -36,40 +36,36 @@ function ikrepseli() {
     document.getElementById("messages").innerHTML =
       "Jūs negalite pridėti šio kiekio";
   }
- 
-      
+}
+document.getElementById("reset").onclick = function () {
+  document.getElementById("kiekis").value = "1";
+  document.getElementById("messages").innerHTML = "";
+};
 
-  }
-  document.getElementById('reset').onclick = function() {   
-    document.getElementById("kiekis").value = '1';
-    document.getElementById("messages").innerHTML =
-      " ";
-   
-  }
-
+// Console log duomenu atvaizdavimo startas
 const duomenys = [
-    {
-    vardas : 'petras',
-    pavarde : 'petraitis',
-    gimtadienis : '1992-07-20',
-    lytis : 'vyras'
-    },
+  {
+    vardas: "petras",
+    pavarde: "petraitis",
+    gimtadienis: "1992-07-20",
+    lytis: "vyras",
+  },
 
-    {
-    vardas : 'jonas',
-    pavarde : 'jonaitis',
-    gimtadienis : '1990-06-13',
-    lytis : 'vyras'
-    },
+  {
+    vardas: "jonas",
+    pavarde: "jonaitis",
+    gimtadienis: "1990-06-13",
+    lytis: "vyras",
+  },
 
-    {
-    vardas : 'janina',
-    pavarde : 'janiene',
-    gimtadienis : '1996-04-15',
-    lytis : 'moteris'
-    }
+  {
+    vardas: "janina",
+    pavarde: "janiene",
+    gimtadienis: "1996-04-15",
+    lytis: "moteris",
+  },
 ];
- 
+
 // for(let eile in duomenys){
 //     console.log(duomenys[eile]['vardas']);
 //     console.log(duomenys[eile]['pavarde']);
@@ -77,8 +73,48 @@ const duomenys = [
 //     console.log(duomenys[eile]['lytis']);
 // }
 
-for(let eile in duomenys) {
-    console.log(duomenys[eile]['vardas'], duomenys[eile]['pavarde'], 
-    duomenys[eile]['gimtadienis'], duomenys[eile]['lytis']);
+// for(let eile in duomenys) {
+//     console.log(duomenys[eile]['vardas'], duomenys[eile]['pavarde'],
+//     duomenys[eile]['gimtadienis'], duomenys[eile]['lytis']);
 
+// }
+
+for (let indeksas in duomenys) {
+  let eilute = "";
+
+  for (let tekstas in duomenys[indeksas]) {
+    eilute += duomenys[indeksas][tekstas] + " ";
+  }
+
+  console.log(eilute);
 }
+// Console log duomenu atvaizdavimo pabaiga
+
+// Easy jquery table log print start
+var dataSet = [
+    ["Petras", 
+    "Petraitis", 
+    "1992-07-20", 
+    "Vyras"],
+    ["Jonas", 
+    "Jonaitis", 
+    "1990-06-13", 
+    "Vyras"],
+    ["Janina", 
+    "Jonaitiene", 
+    "1996-04-15", 
+    "Moteris"]
+];
+
+$(document).ready(function () {
+  $("#asmenineInfo").DataTable({
+    data: dataSet,
+    columns: [
+      { title: "Vardas" },
+      { title: "pavarde" },
+      { title: "Gimtadienis" },
+      { title: "lytis" },
+    ],
+  });
+});
+// Easy jquery table log print end
