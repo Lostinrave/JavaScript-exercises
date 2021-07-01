be8adb9b4b636b89c299246c4201bfca5d102dfb
Lostinrave <83598946+Lostinrave@users.noreply.github.com>
2021-07-01 17:41:49 +0300

    added different 'message' to error returns

--- assets/JS/custom.js
+++ assets/JS/custom.js
@@ -7,34 +7,35 @@
 //     "You succesfuly added " + cartValue + " " + word + " to your shoping cart";
 // }
 
-document.getElementById('kvietejas').addEventListener("click", ikrepseli);
+document.getElementById("kvietejas").addEventListener("click", ikrepseli);
 
 function ikrepseli() {
+  var kiekis = document.getElementById("kiekis").value;
+  var zodis = "prekes";
 
-    var kiekis = document.getElementById('kiekis').value;
-    var zodis  = 'prekes';
+  if (kiekis % 10 == 1) {
+    zodis = "prekę";
+  }
 
-    if(kiekis%10 == 1) {
-        zodis  = 'prekę';
-    }
+  if ((kiekis > 10 && kiekis < 20) || kiekis % 10 == 0) {
+    zodis = "prekių";
+  }
+  document.getElementById("messages").innerHTML =
+    "Jūs sėkmingai pridėjote " + kiekis + " " + zodis + "  į savo krepšelį!";
+  if (kiekis > 100) {
+    var error = document.getElementById("kiekis");
+    error.classList.add("error");
+    alert("Jūsų pasirinktas prekių kiekis yra didesnis nei leistinas");
+    document.getElementById("messages").innerHTML =
+      "Jūs negalite pridėti šio kiekio";
+  }
+  if (kiekis < 1) {
+    var error = document.getElementById("kiekis");
+    error.classList.add("error");
+    alert("Pasirinktas per mažas kiekis");
+    document.getElementById("messages").innerHTML =
+      "Jūs negalite pridėti šio kiekio";
+  }
 
-    if( (kiekis > 10 && kiekis < 20) || kiekis%10 == 0) {
-        zodis = 'prekių';
-    } 
-     if (kiekis > 100){
-        var error = document.getElementById('kiekis');
-        error.classList.add('error');
-        alert("Jūsų pasirinktas prekių kiekis yra didesnis nei leistinas");
-     }
-     if (kiekis < 1) {
-        var error = document.getElementById('kiekis');
-        error.classList.add('error');
-        alert("Pasirinktas per mažas kiekis");
-
-     }
-   
-
-    document.getElementById('messages').innerHTML = 'Jūs sėkmingai pridėjote ' + kiekis + ' ' + zodis + '  į savo krepšelį!';
-
-    //alert('Jūs sėkmingai pridėjote ' + kiekis + ' prekę į savo krepšelį!');
+  //alert('Jūs sėkmingai pridėjote ' + kiekis + ' prekę į savo krepšelį!');
 }
